af9395caa4f85c2a31b3cec1f6af9b332861242b
AdrianIlaga <adrianilaga771@gmail.com>
2021-05-17 16:45:33 +0800

    MVP achieved

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
 
-    validates :title, presence: true
+    validates :title, presence: true,
+                      uniqueness: true
     validates :body,  presence: true,
                       length: {minimum: 5}
 
